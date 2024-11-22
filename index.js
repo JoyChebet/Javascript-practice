fc562300637ff045b7ee15c9b16d66e462347d14
Joy Chebet <160256031+JoyChebet@users.noreply.github.com>
2024-11-22 08:35:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// let username;
+// let password;
+
+
+// document.getElementById("mysubmit").onclick = function(){
+//     username = document.getElementById("mytext").value;
+//     password = document.getElementById("mytext2").value;
+//     console.log(username);
+//     console.log(password);
+// }
+
+let age = window.prompt("How old are you");
+age = Number(age);
+age +=10;
+
+console.log(age, typeof age);
